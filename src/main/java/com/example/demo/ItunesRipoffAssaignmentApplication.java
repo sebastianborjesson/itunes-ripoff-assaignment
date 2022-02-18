7ffe3941d681d23746c7da8f47aacb282eab2349
sebastianborjesson <sebastian.borjesson@live.se>
2022-02-18 16:35:36 +0100

    Feat: inital commit

---
+++ src/main/java/com/example/demo/ItunesRipoffAssaignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItunesRipoffAssaignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItunesRipoffAssaignmentApplication.class, args);
+	}
+
+}
